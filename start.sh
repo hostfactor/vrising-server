de1882fcfec6c998de67bf4775bac6e2098aaecf
edwardowens <eddiebeme@gmail.com>
2022-06-09 23:52:20 -0700

    feat(docker): Adding env vars.

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 Xvfb :0 -screen 0 1024x768x16 &
 
-DISPLAY=:0.0 wine64 $HOME/vrising/VRisingServer.exe -persistentDataPath $SAVE_DIR -logFile $LOG_FILE -serverName $SERVER_NAME -saveName $SAVE_NAME
+DISPLAY=:0.0 wine64 $HOME/vrising/VRisingServer.exe -persistentDataPath "$SAVE_DIR" -logFile "$LOG_FILE" -serverName "$SERVER_NAME" -saveName "$SAVE_NAME"
